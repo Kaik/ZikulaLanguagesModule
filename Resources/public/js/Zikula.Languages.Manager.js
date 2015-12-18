858c3a5b14d82a98a8ec3e868aa46a3d0b572bc3
Kaik <kapitanogor@googlemail.com>
2015-12-18 04:23:22 +0100

    Add new language ajax action - not finished.

--- Resources/public/js/Zikula.Languages.Manager.js
+++ Resources/public/js/Zikula.Languages.Manager.js
@@ -4,11 +4,15 @@ Zikula.LanguagesManager.Admin = {};
 
 ( function($) {
 	
+    var modal;    
+        
     Zikula.LanguagesManager.Admin.init = function ()
     { 
         
         console.log('stage: 0');
     	
+        modal = $("#languages_manager_modal");
+        
         $("#language_manager_add_language").click(function(e) {
     		e.preventDefault();
     	          Zikula.LanguagesManager.Admin.newLanguage();
@@ -30,7 +34,10 @@ Zikula.LanguagesManager.Admin = {};
             url: Routing.generate('zikulalanguagesmodule_admin_newlanguage'),
             data: pars
         }).success(function(result) {	
-            //var template = result.template;
+                var html = result;
+                modal.find('.modal-title').html('Add new language');
+                modal.find('.modal-body').html(html);
+                modal.modal("show");
        	 	//manager.view.itemEdit( template ); 
         }).error(function(result) {
        	 	//manager.view.displayError( result.status + ': ' + result.statusText);
